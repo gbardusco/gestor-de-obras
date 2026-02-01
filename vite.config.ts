cea77ed6e6be620af4e5d986ab77bc6f0515b6fd
Gustavo <bardusco.gustavo@gmail.com>
2026-02-01 01:59:42 -0300

    adição de funcionalidades novas de linha do tempo e planner

--- vite.config.ts
+++ vite.config.ts
@@ -23,7 +23,7 @@ export default defineConfig(({ mode }) => {
         outDir: 'dist',
         assetsDir: 'assets',
         sourcemap: false,
-        minify: 'terser',
+        minify: false,
         rollupOptions: {
           output: {
             manualChunks: undefined,
